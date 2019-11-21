d8f07034c466257f188b7a2671de10387c9cda61
Разгуляева Анастасия Дмитриевна OUT <Razgulyaeva.An.Dm@sberbank.ru>
2019-11-21 15:03:06 +0300

    initial commit

---
+++ src/test/java/com/accenture/springdata/SpringdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
